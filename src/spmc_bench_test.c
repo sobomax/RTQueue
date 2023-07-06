a153757037e34af0dfd853140c4c256c8fae9cf2
Maksym Sobolyev <sobomax@sippysoft.com>
2023-07-05 21:27:19 -0700

    Add -t argument to run for a given number of seconds, fix some bugs on 32-bit platforms.

--- src/spmc_bench_test.c
+++ src/spmc_bench_test.c
@@ -17,6 +17,7 @@
 #define NUM_SECONDS 10
 #define QUEUE_SIZE 4096
 #define WRKR_BATCH_SIZE 8
+#define EOF_SENTINEL ((uintptr_t)-1)
 
 typedef struct {
     SPMCQueue* queue;
@@ -31,7 +32,7 @@ void* worker_thread(void* arg) {
     WorkerArgs* args = (WorkerArgs*) arg;
     SPMCQueue* queue = args->queue;
     _Alignas(CACHE_LINE_SIZE) void* values[WRKR_BATCH_SIZE] = {};
-    uint64_t last_value = 0;
+    uintptr_t last_value = 0;
     struct timespec delay = {};
     int sleepcycles = 0;
 
@@ -39,12 +40,12 @@ void* worker_thread(void* arg) {
         size_t n = try_pop_many(queue, values, WRKR_BATCH_SIZE);
         if (likely(n > 0)) {
             for (size_t i = 0; i < n; i++) {
-                if (unlikely(values[i] == (void*)-1)) {
+                if (unlikely(values[i] == EOF_SENTINEL)) {
                     goto out;
                 }
-                uint64_t current_value = (uint64_t)values[i];
-                if (unlikely(current_value <= last_value)) {
-                    printf("Error: Expected value greater than %" PRIu64 " but got %" PRIu64 "\n", last_value, current_value);
+                uintptr_t current_value = (uintptr_t)values[i];
+                if (unlikely(current_value <= last_value && !(last_value + 1 == EOF_SENTINEL && current_value == EOF_SENTINEL + 1))) {
+                    printf("Error: Expected value greater than %" PRIuPTR " but got %" PRIuPTR "\n", last_value, current_value);
                     abort();
                     exit(EXIT_FAILURE);
                 }
@@ -74,11 +75,28 @@ out:
 #define timespec2dtime(s) ((double)SEC(s) + \
   (double)NSEC(s) / 1000000000.0)
 
-int main() {
+int main(int argc, char *argv[]) {
     SPMCQueue* queue = create_queue(QUEUE_SIZE);
     pthread_t worker;
     WorkerArgs args = {.queue = queue};
     struct timespec st = {}, et = {};
+    int num_seconds = NUM_SECONDS; // default
+    int opt;
+
+    while ((opt = getopt(argc, argv, "t:")) != -1) {
+        switch (opt) {
+        case 't':
+            num_seconds = atoi(optarg);
+            if (num_seconds <= 0) {
+                fprintf(stderr, "Number of seconds must be greater than 0\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-t num_seconds]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
 
     if (pthread_create(&worker, NULL, worker_thread, &args)) {
         fprintf(stderr, "Error creating thread\n");
@@ -86,13 +104,15 @@ int main() {
     }
 
     clock_gettime(CLOCK_MONOTONIC, &st);
-    double stime = timespec2dtime(&st) + NUM_SECONDS;
+    double stime = timespec2dtime(&st) + num_seconds;
     double etime = 0;
-    uintptr_t i = 0;
+    uint64_t i;
     uint64_t disc = 0;
     uint64_t chksum = 0;
-    for (;;) {
-        while (unlikely(!try_push(queue, (void*) i + 1))) {
+    for (i = 1;;i++) {
+        if (sizeof(uintptr_t) < 8 && unlikely(((uintptr_t)i) == EOF_SENTINEL))
+            i++;
+        while (unlikely(!try_push(queue, (void*) i))) {
             struct timespec delay = {.tv_nsec = 1};
             nanosleep(&delay, NULL);
             void *junk;
@@ -101,16 +121,15 @@ int main() {
                 disc++;
             }
         }
-        chksum += i + 1;
+        chksum += (uintptr_t)i;
         if (unlikely((((1 << 16) - 1) & i) == 0)) {
             clock_gettime(CLOCK_MONOTONIC, &et);
             etime = timespec2dtime(&et);
             if (etime >= stime)
                 break;
         }
-        i++;
     }
-    while (!try_push(queue, (void*)-1)) { pthread_yield(); } // Add EOF marker
+    while (!try_push(queue, (void *)EOF_SENTINEL)) { pthread_yield(); } // Add EOF marker
 
     // Wait for the worker thread to exit
     if (pthread_join(worker, NULL)) {
@@ -119,7 +138,8 @@ int main() {
     }
 
     assert(chksum == args.chksum);
-    double ttime = etime - stime + NUM_SECONDS;
+    double ttime = etime - stime + num_seconds;
+    i--;
     printf("Sent %" PRIu64 " + %" PRIu64 ", received %" PRIu64 " messages in %.5f seconds\n", i - disc, disc, args.count, ttime);
     printf("PPS is %.3f MPPS, packet loss rate %.4f%%\n", 1e-6 * (double)(i - disc) / ttime, 100.0 * (double)disc / (double)i);
 
