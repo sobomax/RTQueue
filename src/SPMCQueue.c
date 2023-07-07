d70b43259b120bf78b8a57e19f6e0a2d9b66ebaf
Maksym Sobolyev <sobomax@sippysoft.com>
2023-07-06 20:29:38 -0700

    The readIdxCache is not shared, so it does not need to be handled atomically.

--- src/SPMCQueue.c
+++ src/SPMCQueue.c
@@ -17,7 +17,7 @@ struct SPMCQueue {
     size_t capacity;
     uint64_t mask;
     _Alignas(CACHE_LINE_SIZE) _Atomic uint64_t writeIdx;
-    _Alignas(CACHE_LINE_SIZE) _Atomic uint64_t readIdxCache;
+    _Alignas(CACHE_LINE_SIZE) uint64_t readIdxCache;
     _Alignas(CACHE_LINE_SIZE) _Atomic uint64_t readIdx;
     _Alignas(CACHE_LINE_SIZE) _Atomic uint64_t writeIdxCache;
     _Alignas(CACHE_LINE_SIZE) void* slots[0]; // FAM for void pointer type slots
@@ -36,7 +36,7 @@ create_queue(size_t capacity)
     atomic_init(&queue->writeIdx, 0);
     atomic_init(&queue->readIdx, 0);
     atomic_init(&queue->writeIdxCache, 0);
-    atomic_init(&queue->readIdxCache, 0);
+    queue->readIdxCache = 0;
     return queue;
 }
 
@@ -49,8 +49,6 @@ void destroy_queue(SPMCQueue* queue) {
     (atomic_load_explicit(&(q)->readIdx,             (mo)))
 #define LOAD_W_IDX(q, mo) \
     (atomic_load_explicit(&(q)->writeIdx,            (mo)))
-#define LOAD_R_CACHE(q)   \
-    (atomic_load_explicit(&(q)->readIdxCache,        memory_order_relaxed))
 #define LOAD_W_CACHE(q)   \
     (atomic_load_explicit(&(q)->writeIdxCache,       memory_order_relaxed))
 #define UPDATE_R_IDX(q, ov, nv) \
@@ -59,8 +57,6 @@ void destroy_queue(SPMCQueue* queue) {
                                                      memory_order_relaxed))
 #define UPDATE_W_IDX(q, v) \
     (atomic_store_explicit(&(q)->writeIdx,      (v), memory_order_release))
-#define UPDATE_R_CACHE(q, v) \
-    (atomic_store_explicit(&(q)->readIdxCache,  (v), memory_order_relaxed))
 #define UPDATE_W_CACHE(q, v) \
     (atomic_store_explicit(&(q)->writeIdxCache, (v), memory_order_relaxed))
 
@@ -72,17 +68,15 @@ try_push(SPMCQueue* queue, void* value)
     uint64_t writeIdx = LOAD_W_IDX(queue, memory_order_relaxed);
     uint64_t nextWriteIdx = writeIdx + 1;
     // If the queue is not full
-    uint64_t readIdxCache = LOAD_R_CACHE(queue);
-    uint64_t newsize = nextWriteIdx - readIdxCache;
+    uint64_t newsize = nextWriteIdx - queue->readIdxCache;
     if(newsize <= queue->capacity) {
         queue->slots[writeIdx & queue->mask] = value;
         UPDATE_W_IDX(queue, nextWriteIdx);
         return true;
     }
     // Update the cached index and retry
-    readIdxCache = LOAD_R_IDX(queue, memory_order_acquire);
-    UPDATE_R_CACHE(queue, readIdxCache);
-    newsize = nextWriteIdx - readIdxCache;
+    queue->readIdxCache = LOAD_R_IDX(queue, memory_order_acquire);
+    newsize = nextWriteIdx - queue->readIdxCache;
     if(newsize <= queue->capacity) {
         queue->slots[writeIdx & queue->mask] = value;
         UPDATE_W_IDX(queue, nextWriteIdx);
