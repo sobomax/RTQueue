227156225e0062e9240ff9f6085f8e5f31b1d7aa
Maksym Sobolyev <sobomax@sippysoft.com>
2026-06-12 00:17:09 -0700

    Make this 1.2.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup (name = 'RTQueue',
-       version = '1.1',
+       version = '1.2',
        description = 'This is a package for LossyQueue module',
        ext_modules = [module1, module2],
        cmdclass={'test': PyTestCommand} if PyTestCommand else {},
